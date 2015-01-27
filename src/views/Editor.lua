60a3f258fc15ac42fcbccd25b41e15b957b7ea5a
chrisdugne <chris.dugne@uralys.com>
2015-01-27 21:58:04 +0100

    grid snapping

--- src/views/Editor.lua
+++ src/views/Editor.lua
@@ -38,6 +38,10 @@ function prepareScreen()
 
     --------------------------------------
 
+    app.screen.gridVisible = false
+
+    --------------------------------------
+
     -- prepare for toucheController dragging
     app.screen.markX   = 0
     app.screen.markY   = 0
@@ -97,20 +101,42 @@ function prepareToolbar()
     app.toolbarBounds:setStrokeColor( 225/255, 225/255, 225/255 )
 
     --------------------------------------
+    -- Button to center
 
     local toCenter = display.newImage(
         app.toolbar,
         'assets/images/editor/noun_33953_cc.png',
-        10, TOOLBAR_HEIGHT/2
+        buttonX(1), TOOLBAR_HEIGHT/2
     )
 
     toCenter.anchorX = 0
-    toCenter:scale(0.8, 0.8)
 
     utils.onTouch(toCenter, function()
         centerLevel()
     end)
+
+    --------------------------------------
+    -- Button toggle grid snapping
+
+    local snapping = display.newImage(
+        app.toolbar,
+        'assets/images/editor/snap-grid.png',
+        buttonX(2), TOOLBAR_HEIGHT/2
+    )
+
+    snapping.anchorX = 0
+
+    utils.onTouch(snapping, function()
+        app.screen.gridVisible = not app.screen.gridVisible
+        if(app.screen.gridVisible) then
+            showSnapGrid()
+        else
+            hideSnapGrid()
+        end
+    end)
 end
+
+--------------------------------------------------------------------------------
 
 function centerLevel()
     app.screen.level.x = SCREEN_WIDTH/2
@@ -136,6 +162,54 @@ function prepareMenu()
     app.itemsBounds.strokeWidth = 1
     app.itemsBounds:setFillColor( 0.12 )
     app.itemsBounds:setStrokeColor( 225/255, 225/255, 225/255 )
+end
+
+--------------------------------------------------------------------------------
+
+function buttonX(position)
+    return 10 + 70 * ( position - 1 )
+end
+
+--------------------------------------------------------------------------------
+
+function hideSnapGrid()
+    display.remove(app.screen.grid)
+    app.screen.grid = nil
+end
+
+function showSnapGrid()
+    app.screen.grid = display.newGroup()
+    app.screen.grid.lines = {}
+
+    for i=1,41 do
+        local hLine = display.newLine(
+            app.screen.grid,
+            -Room.WIDTH*20,
+            Room.HEIGHT * (i-21),
+            Room.WIDTH*20,
+            Room.HEIGHT * (i-21)
+        )
+
+        local vLine = display.newLine(
+            app.screen.grid,
+            Room.WIDTH * (i-21),
+            -Room.HEIGHT*20,
+            Room.WIDTH * (i-21),
+            Room.HEIGHT*20
+        )
+
+        hLine.stroke.effect = "generator.marchingAnts"
+        hLine.strokeWidth = 2
+        hLine:setStrokeColor( 245/255, 245/255, 245/255 )
+
+        vLine.stroke.effect = "generator.marchingAnts"
+        vLine.strokeWidth = 2
+        vLine:setStrokeColor( 245/255, 245/255, 245/255 )
+
+    end
+
+    app.screen.level:insert(app.screen.grid)
+
 end
 
 --------------------------------------------------------------------------------
